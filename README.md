b5d4b876bd85a7676c5725de3e3eaaab72ff929b
lubna151 <115445733+lubna151@users.noreply.github.com>
2022-10-10 12:53:25 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nativescript-stackblitz-templates-92bfde
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/nativescript-stackblitz-templates-92bfde)
---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { Button, StyleSheet, View } from 'react-native';
+
+export default class ButtonBasics extends Component {
+  _onPressButton() {
+    alert('You tapped the button!');
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.buttonContainer}>
+          <Button onPress={this._onPressButton} title="Press Me" />
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button
+            onPress={this._onPressButton}
+            title="Press Me"
+            color="#841584"
+          />
+        </View>
+        <View style={styles.alternativeLayoutButtonContainer}>
+          <Button onPress={this._onPressButton} title="This looks great!" />
+          <Button onPress={this._onPressButton} title="OK!" color="#841584" />
+        </View>
+      </View>
+    );
+  }
+}
